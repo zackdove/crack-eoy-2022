3e34010495be87f1aeb91b27830b139e688bac14
Zack Dove <zdove@live.co.uk>
2024-01-08 18:58:16 +0000

    Update

---
+++ public/assets/js/gridConfig-22.js
@@ -0,0 +1,114 @@
+// Pls dont put more than six
+
+const gridConfig = {
+    projects: [
+        {
+            title: 'TOP ALBUMS 2022',
+            coordinates: `41°24'12.2" N 2°10'26.5"`,
+            position: {
+                x: 0,
+                y: -3,
+            },
+            link: 'https://crackmagazine.net/article/list-article/best-albums-2022/',
+            enabled: true,
+            textures: [
+                '/wp-content/themes/crack/aesthetics/assets/images/sudanArchives.jpeg',
+                '/wp-content/themes/crack/aesthetics/assets/images/bjork.jpeg',
+                '/wp-content/themes/crack/aesthetics/assets/images/lsdxoxo.jpeg',
+            ],
+        },
+        {
+            title: 'TOP TRACKS 2022',
+            coordinates: `41°24'12.2" N 2°10'26.5"`,
+            position: {
+                x: -1.5,
+                y: -2,
+            },
+            textureType: 'video',
+            textureSrc: '/assets/videos/crack_demo.mp4#t=0.5#t=0.5',
+            link: 'https://crackmagazine.net/article/list-article/best-songs-2022/',
+            textureKey: 'lsdxoxo',
+            enabled: true,
+            textures: [
+                '/wp-content/themes/crack/aesthetics/assets/images/sudanArchives.jpeg',
+                '/wp-content/themes/crack/aesthetics/assets/images/bjork.jpeg',
+                '/wp-content/themes/crack/aesthetics/assets/images/lsdxoxo.jpeg',
+            ],
+        },
+        {
+            title: 'YEAR IN MIXES',
+            coordinates: `41°24'12.2" N 2°10'26.5"`,
+            position: {
+                x: 1.5,
+                y: 1.5,
+            },
+            textureType: 'video',
+            textureSrc: '/assets/videos/crack_demo.mp4#t=0.5#t=0.5',
+            link: 'https://crackmagazine.net/article/long-reads/the-year-in-mixes-2022/',
+            textureKey: 'arca',
+            enabled: true,
+            textures: [
+                '/wp-content/themes/crack/aesthetics/assets/images/sudanArchives.jpeg',
+                '/wp-content/themes/crack/aesthetics/assets/images/bjork.jpeg',
+                '/wp-content/themes/crack/aesthetics/assets/images/lsdxoxo.jpeg',
+            ],
+        },
+        {
+            title: 'STAFF HIGHLIGHTS',
+            coordinates: `41°24'12.2" N 2°10'26.5"`,
+            position: {
+                x: -2,
+                y: 2,
+            },
+            textureType: 'video',
+            textureSrc: '/assets/videos/crack_demo.mp4#t=0.5#t=0.5',
+            link: 'https://crackmagazine.net/article/list-article/staff-lists-crack-magazine-2022/',
+            textureKey: 'arca',
+            enabled: true,
+            textures: [
+                '/wp-content/themes/crack/aesthetics/assets/images/sudanArchives.jpeg',
+                '/wp-content/themes/crack/aesthetics/assets/images/bjork.jpeg',
+                '/wp-content/themes/crack/aesthetics/assets/images/lsdxoxo.jpeg',
+            ],
+        },
+        {
+            title: 'GIFT GUIDE',
+            coordinates: `41°24'12.2" N 2°10'26.5"`,
+            position: {
+                x: -3,
+                y: -1,
+            },
+            textureType: 'video',
+            textureSrc: '/assets/videos/crack_demo.mp4#t=0.5#t=0.5',
+            link: 'https://crackmagazine.net/article/lists/christmas-gift-guide-2022/',
+            textureKey: 'arca',
+            enabled: true,
+            textures: [
+                '/wp-content/themes/crack/aesthetics/assets/images/sudanArchives.jpeg',
+                '/wp-content/themes/crack/aesthetics/assets/images/bjork.jpeg',
+                '/wp-content/themes/crack/aesthetics/assets/images/lsdxoxo.jpeg',
+            ],
+        },
+        {
+            title: '2022 ON SPOTIFY',
+            coordinates: `41°24'12.2" N 2°10'26.5"`,
+            position: {
+                x: 1.75,
+                y: 0,
+            },
+            textureType: 'video',
+            textureSrc: '/assets/videos/crack_demo.mp4#t=0.5#t=0.5',
+            link: 'https://crackmagazine.net/2022/12/crack-mag-top-25-2022-playlist/',
+            textureKey: 'arca',
+            enabled: true,
+            textures: [
+                '/wp-content/themes/crack/aesthetics/assets/images/sudanArchives.jpeg',
+                '/wp-content/themes/crack/aesthetics/assets/images/bjork.jpeg',
+                '/wp-content/themes/crack/aesthetics/assets/images/lsdxoxo.jpeg',
+            ],
+        },
+
+
+
+    ]
+}
