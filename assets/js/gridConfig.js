7f08d4b84f3fea520ff49a9de8714fecf10ce5d2
zackdove <zackdove@users.noreply.github.com>
2024-01-08 18:58:57 +0000

    deploy: 3e34010495be87f1aeb91b27830b139e688bac14

--- assets/js/gridConfig.js
+++ assets/js/gridConfig.js
@@ -4,156 +4,211 @@ const gridConfig = {
     gems: {
         textures: [
 
-            "/assets/images/gems/tear.png",
-            "/assets/images/postits/orange.png",
-            "/assets/images/postits/green.png",
-            "/assets/images/postits/pink.png",
-            "/assets/images/gems/heart.png",
-          
-            "/assets/images/postits/purple.png",
-            "/assets/images/gems/star.png",
+            "/wp-content/themes/crack/aesthetics/crack-eoy-23-assets/assets/images/gems/tear.png",
+            "/wp-content/themes/crack/aesthetics/crack-eoy-23-assets/assets/images/gems/star.png",
+            "/wp-content/themes/crack/aesthetics/crack-eoy-23-assets/assets/images/postits/green.png",
+            "/wp-content/themes/crack/aesthetics/crack-eoy-23-assets/assets/images/postits/pink.png",
+            "/wp-content/themes/crack/aesthetics/crack-eoy-23-assets/assets/images/gems/heart.png",
+
+            "/wp-content/themes/crack/aesthetics/crack-eoy-23-assets/assets/images/postits/purple.png",
 
 
-          
-            
-           
-            "/assets/images/gems/yellow.png",
-            
-            "/assets/images/gems/purple.png",
-           
-            "/assets/images/postits/green.png",
-          
-           
-            "/assets/images/postits/purple.png",
-    ],
+
+
+            "/wp-content/themes/crack/aesthetics/crack-eoy-23-assets/assets/images/postits/orange.png",
+
+
+            "/wp-content/themes/crack/aesthetics/crack-eoy-23-assets/assets/images/gems/yellow.png",
+
+            "/wp-content/themes/crack/aesthetics/crack-eoy-23-assets/assets/images/gems/purple.png",
+
+            "/wp-content/themes/crack/aesthetics/crack-eoy-23-assets/assets/images/postits/green.png",
+
+
+            "/wp-content/themes/crack/aesthetics/crack-eoy-23-assets/assets/images/postits/purple.png",
+        ],
         textureKeys: []
     },
     projects: [
         {
-            title: 'Top 50 Albums 2023',
+            title: 'The Top 50 Albums of the Year',
             coordinates: `41°24'12.2" N 2°10'26.5"`,
             position: {
                 x: -0.25,
+                y: 1.25,
+            },
+            mobilePosition: {
+                x: -1.25,
                 y: 0.75,
             },
-            mobilePosition:{
-                x: -0.95,
-                y: 0.75,
-            },
-            link: '/examplelink',
+            link: 'https://crackmagazine.net/article/list-article/best-albums-2023/',
             enabled: true,
             textures: [
-                '/assets/images/sudanArchives.jpeg',
-                '/assets/images/lsdxoxo.jpeg',
+                '/wp-content/themes/crack/aesthetics/crack-eoy-23-assets/assets/images/albums/EOY-Portal-Albums-Hover-1.jpg',
+                '/wp-content/themes/crack/aesthetics/crack-eoy-23-assets/assets/images/albums/EOY-Portal-Albums-Hover-3.jpg',
+                '/wp-content/themes/crack/aesthetics/crack-eoy-23-assets/assets/images/albums/EOY-Portal-Albums-Hover-10.jpg',
             ],
         },
         {
-            title: 'Top 50 Albums 2023',
+            title: 'The Top 25 Tracks of the Year',
             coordinates: `41°24'12.2" N 2°10'26.5"`,
             position: {
                 x: -0.5,
                 y: -1.5,
             },
-            mobilePosition:{
-                x: 0.4,
+            mobilePosition: {
+                x: 0.6,
                 y: -1.45,
             },
             textureType: 'video',
-            textureSrc: '/assets/videos/crack_demo.mp4#t=0.5#t=0.5',
-            link: '/examplelink',
+            textureSrc: '/wp-content/themes/crack/aesthetics/crack-eoy-23-assets/assets/videos/crack_demo.mp4#t=0.5#t=0.5',
+            link: 'https://crackmagazine.net/article/list-article/best-songs-2023/',
             textureKey: 'lsdxoxo',
             enabled: true,
             textures: [
-                '/assets/images/sudanArchives.jpeg',
-                '/assets/images/lsdxoxo.jpeg',
+                '/wp-content/themes/crack/aesthetics/crack-eoy-23-assets/assets/images/tracks/centralCee.jpg',
+                '/wp-content/themes/crack/aesthetics/crack-eoy-23-assets/assets/images/tracks/mitski.jpg',
+                '/wp-content/themes/crack/aesthetics/crack-eoy-23-assets/assets/images/tracks/bambii.jpg?v=1.01',
             ],
         },
         {
-            title: 'Top 50 Albums 2023',
+            title: 'The Year in Mixes',
             coordinates: `41°24'12.2" N 2°10'26.5"`,
             position: {
                 x: 1.5,
                 y: -2,
             },
-            mobilePosition:{
+            mobilePosition: {
                 x: -0.25,
                 y: -2.25,
             },
             textureType: 'video',
-            textureSrc: '/assets/videos/crack_demo.mp4#t=0.5#t=0.5',
-            link: '/examplelink',
+            textureSrc: '/wp-content/themes/crack/aesthetics/crack-eoy-23-assets/assets/videos/crack_demo.mp4#t=0.5#t=0.5',
+            link: 'https://crackmagazine.net/article/long-reads/2023-the-year-in-mixes/',
             textureKey: 'arca',
             enabled: true,
             textures: [
-                '/assets/images/sudanArchives.jpeg',
-                '/assets/images/lsdxoxo.jpeg',
+                '/wp-content/themes/crack/aesthetics/crack-eoy-23-assets/assets/images/yearinmixes/Greg by Raoul Rampare.jpg',
+                '/wp-content/themes/crack/aesthetics/crack-eoy-23-assets/assets/images/yearinmixes/Salamanda-Parallax.jpg',
+                '/wp-content/themes/crack/aesthetics/crack-eoy-23-assets/assets/images/yearinmixes/Tra-Tra-Tra-Parallax-V2-2023.jpg',
             ],
         },
         {
-            title: 'Top 50 Albums 2023',
+            title: 'The Artists Who Defined the Year',
             coordinates: `41°24'12.2" N 2°10'26.5"`,
             position: {
-                x: -2,
-                y: 0.5,
+                x: 1.0,
+                y: -3,
             },
-            mobilePosition:{
-                x: 0.25,
+            mobilePosition: {
+                x: -0.25,
+                y: -2.25,
+            },
+            textureType: 'video',
+            textureSrc: '/wp-content/themes/crack/aesthetics/crack-eoy-23-assets/assets/videos/crack_demo.mp4#t=0.5#t=0.5',
+            link: 'https://crackmagazine.net/tag/signing-off/',
+            textureKey: 'arca',
+            enabled: true,
+            textures: [
+                '/wp-content/themes/crack/aesthetics/crack-eoy-23-assets/assets/images/topartists/2cp_1.jpeg?v=1.01',
+                '/wp-content/themes/crack/aesthetics/crack-eoy-23-assets/assets/images/topartists/corner.jpeg?v=1.01',
+                '/wp-content/themes/crack/aesthetics/crack-eoy-23-assets/assets/images/topartists/marina.jpeg?v=1.01',
+                '/wp-content/themes/crack/aesthetics/crack-eoy-23-assets/assets/images/topartists/nikkinair.jpeg?v=1.01',
+                '/wp-content/themes/crack/aesthetics/crack-eoy-23-assets/assets/images/topartists/slauson.jpeg?v=1.01',
+            ],
+        },
+        {
+            title: 'Staff Lists',
+            coordinates: `41°24'12.2" N 2°10'26.5"`,
+            position: {
+
+
+                x: 1.3,
+                y: 0.8,
+            },
+            mobilePosition: {
+                x: 0.85,
                 y: 1.25,
             },
             textureType: 'video',
-            textureSrc: '/assets/videos/crack_demo.mp4#t=0.5#t=0.5',
-            link: '/examplelink',
+            textureSrc: '/wp-content/themes/crack/aesthetics/crack-eoy-23-assets/assets/videos/crack_demo.mp4#t=0.5#t=0.5',
+            link: 'https://crackmagazine.net/article/list-article/staff-lists-crack-magazine-2023/',
             textureKey: 'arca',
             enabled: true,
             textures: [
-                '/assets/images/sudanArchives.jpeg',
-                '/assets/images/lsdxoxo.jpeg',
+                '/wp-content/themes/crack/aesthetics/crack-eoy-23-assets/assets/images/stafflists/bendik.jpeg',
+                '/wp-content/themes/crack/aesthetics/crack-eoy-23-assets/assets/images/stafflists/eartheater.jpeg',
+                '/wp-content/themes/crack/aesthetics/crack-eoy-23-assets/assets/images/stafflists/luke.png',
             ],
         },
         {
-            title: 'Top 50 Albums 2023',
+            title: 'Christmas Gift Guide',
             coordinates: `41°24'12.2" N 2°10'26.5"`,
             position: {
                 x: -2,
-                y: -2.25,
+                y: -2.55,
             },
-            mobilePosition:{
+            mobilePosition: {
                 x: -0.25,
                 y: 1.85,
             },
             textureType: 'video',
-            textureSrc: '/assets/videos/crack_demo.mp4#t=0.5#t=0.5',
-            link: '/examplelink',
+            textureSrc: '/wp-content/themes/crack/aesthetics/crack-eoy-23-assets/assets/videos/crack_demo.mp4#t=0.5#t=0.5',
+            link: 'https://crackmagazine.net/article/list-article/gift-guide-2023/',
             textureKey: 'arca',
             enabled: true,
             textures: [
-                '/assets/images/sudanArchives.jpeg',
-                '/assets/images/lsdxoxo.jpeg',
+                '/wp-content/themes/crack/aesthetics/crack-eoy-23-assets/assets/images/gifts/hoodie.jpeg?v=1.01',
+                '/wp-content/themes/crack/aesthetics/crack-eoy-23-assets/assets/images/gifts/lostmary.jpeg?v=1.01',
+                '/wp-content/themes/crack/aesthetics/crack-eoy-23-assets/assets/images/gifts/scard.png?v=1.01',
+                '/wp-content/themes/crack/aesthetics/crack-eoy-23-assets/assets/images/gifts/thong.jpeg?v=1.01',
             ],
         },
         {
-            title: 'Top 50 Albums 2023',
+            title: '2023 Mixes',
             coordinates: `41°24'12.2" N 2°10'26.5"`,
             position: {
                 x: 1.75,
                 y: 1.75,
             },
-            mobilePosition:{
-                x: -1.25,
-                y: -2.75,
+            mobilePosition: {
+                x: -1.65,
+                y: -2.55,
             },
             textureType: 'video',
-            textureSrc: '/assets/videos/crack_demo.mp4#t=0.5#t=0.5',
+            textureSrc: '/wp-content/themes/crack/aesthetics/crack-eoy-23-assets/assets/videos/crack_demo.mp4#t=0.5#t=0.5',
+            link: 'https://crackmagazine.net/listen/?collection=2023+Mixes',
+            textureKey: 'arca',
+            enabled: true,
+            textures: [
+                '/wp-content/themes/crack/aesthetics/crack-eoy-23-assets/assets/images/2023mixes/Digital-EOY-Portal-2023-Mixes-Hover-1.jpg',
+                '/wp-content/themes/crack/aesthetics/crack-eoy-23-assets/assets/images/2023mixes/Digital-EOY-Portal-2023-Mixes-Hover-2.jpg',
+                '/wp-content/themes/crack/aesthetics/crack-eoy-23-assets/assets/images/2023mixes/Digital-EOY-Portal-2023-Mixes-Hover-3.jpg',
+            ],
+        },
+
+        {
+            title: 'Stories Behind The Year\'s Best Music Videos',
+            coordinates: `41°24'12.2" N 2°10'26.5"`,
+            position: {
+                x: -2.4,
+                y: 0.5,
+            },
+            mobilePosition: {
+                x: -0.9,
+                y: -3.2,
+            },
+            textureType: 'video',
+            textureSrc: '/wp-content/themes/crack/aesthetics/crack-eoy-23-assets/assets/videos/crack_demo.mp4#t=0.5#t=0.5',
             link: '/examplelink',
             textureKey: 'arca',
             enabled: false,
             textures: [
-                '/assets/images/sudanArchives.jpeg',
-                '/assets/images/lsdxoxo.jpeg',
+                '/wp-content/themes/crack/aesthetics/crack-eoy-23-assets/assets/images/albums/EOY-Portal-Albums-Hover-1.jpg',
             ],
         },
 
-       
+
 
 
 
